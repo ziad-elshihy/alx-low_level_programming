2bee8f801c4a539056a14dfea6019fed905fcacd
ziad-elshihy <elshihyziad@gmail.com>
2023-03-21 15:41:40 -0700

    ......

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - This is a absolute value
+ *
+ * @a: This is the entry
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (a * -1);
+	}
+	else
+	{
+		return (a);
+	}
+}
