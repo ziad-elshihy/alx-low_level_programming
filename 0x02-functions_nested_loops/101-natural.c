a0b5f4acfbf3638ce08479b0261f4667dd1ada61
ziad-elshihy <elshihyziad@gmail.com>
2023-03-21 16:19:13 -0700

    ...

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
+		int i, sum = 0;
 
-	return (0);
+		for (i = 0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum += i;
+		}
+
+		printf("%d\n", sum);
+
+		return (0);
 }
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,6 +8,22 @@
  */
 int main(void)
 {
+		int count;
+		unsigned long fib1 = 0, fib2 = 1, sum;
 
-	return (0);
+		for (count = 0; count < 50; count++)
+		{
+			sum = fib1 + fib2;
+			printf("%lu", sum);
+
+			fib1 = fib2;
+			fib2 = sum;
+
+			if (count == 49)
+				printf("\n");
+			else
+				printf(", ");
+		}
+
+		return (0);
 }
